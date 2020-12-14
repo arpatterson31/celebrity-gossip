9ed3c3c8c945323f08c6b93e8cdf479860beadb7
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2020-12-13 21:18:20 -0800

    added mouseover and mouse out effect in javascript

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("dear lord help me...")
+
+//creating new variables for the mouseover and out effect for the home button in the nav
+var mouseOver
+var mouseOut
+
+document.getElementById("home").addEventListener("mouseover", mouseOver);
+document.getElementById("home").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("home").style.color = "purple";
+}
+function mouseOut() {
+    document.getElementById("home").style.color = "black";
+}
+
+
+
